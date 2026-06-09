a74686cae1518bf8bfe94fae47c6396b7c6d5203
cjhih456 <cjhih43@gmail.com>
2026-06-09 15:58:52 +0900

    fix: ingest package path

--- src/ingest/dispatcher/__init__.py
+++ src/ingest/dispatcher/__init__.py
@@ -2,18 +2,26 @@ from src.ingest.dispatcher.feed import build_feed_handler
 from src.ingest.dispatcher.comment import build_comment_handler
 from src.ingest.dispatcher.movie import build_movie_handler
 from src.ingest.dispatcher.utils import Embedder, Handler, _parse_dt
-from src.ingest.dispatcher.index import IngestDispatcher, mock_extract, mock_extract_load_handler, mock_load, default_dispatcher
+from src.ingest.dispatcher.index import (
+    IngestDispatcher,
+    mock_extract,
+    mock_extract_load_handler,
+    mock_load,
+    default_dispatcher,
+    build_production_dispatcher,
+)
 
 __all__ = [
-  "build_feed_handler",
-  "build_comment_handler",
-  "build_movie_handler",
-  "mock_extract",
-  "mock_extract_load_handler",
-  "mock_load",
-  "default_dispatcher",
-  "Embedder",
-  "Handler",
-  "_parse_dt",
-  "IngestDispatcher",
+    "build_production_dispatcher",
+    "build_feed_handler",
+    "build_comment_handler",
+    "build_movie_handler",
+    "mock_extract",
+    "mock_extract_load_handler",
+    "mock_load",
+    "default_dispatcher",
+    "Embedder",
+    "Handler",
+    "_parse_dt",
+    "IngestDispatcher",
 ]
